692f0a7260df5073a286d4790eb6c1125a6ca4b5
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-05-24 14:26:22 +0200

    Update pkce.ts

--- utils/pkce.ts
+++ utils/pkce.ts
@@ -2,19 +2,20 @@
 
 export function generateCodeVerifier(length = 128): string {
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
-  let verifier = '';
+  let text = '';
   for (let i = 0; i < length; i++) {
-    verifier += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return verifier;
+  return text;
 }
 
 export async function generateCodeChallenge(verifier: string): Promise<string> {
   const encoder = new TextEncoder();
   const data = encoder.encode(verifier);
-  const digest = await crypto.subtle.digest('SHA-256', data);
-  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+  const hash = await crypto.subtle.digest('SHA-256', data);
+  const base64String = btoa(String.fromCharCode(...new Uint8Array(hash)))
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=+$/, '');
+  return base64String;
 }
