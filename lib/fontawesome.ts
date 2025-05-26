f1a7210b3444b4e866a68f44ca217fca8bc16ffd
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-05-26 08:13:08 +0200

    Update fontawesome.ts

--- lib/fontawesome.ts
+++ lib/fontawesome.ts
@@ -1,5 +1,6 @@
-// lib/fontawesome.ts
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
 import { faPlay, faPause, faForward, faBackward, faQrcode } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
+config.autoAddCss = false;
 library.add(faPlay, faPause, faForward, faBackward, faQrcode);
