0dea397f318bd166299ac319f0fd36b834003889
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-05-26 08:08:47 +0200

    Create fontawesome.ts

---
+++ lib/fontawesome.ts
@@ -0,0 +1,5 @@
+// lib/fontawesome.ts
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay, faPause, faForward, faBackward, faQrcode } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlay, faPause, faForward, faBackward, faQrcode);
