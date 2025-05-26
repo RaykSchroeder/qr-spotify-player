4b6a9ce73a856aaf0ae9730ac8d57ebe5f10b385
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-05-26 08:22:47 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
 // pages/_app.tsx
-import '../lib/fontawesome';
+import type { AppProps } from 'next/app';
 
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
