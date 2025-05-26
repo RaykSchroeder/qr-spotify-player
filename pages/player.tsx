ac90f3ca838f9467c194631521616c4b8cf60072
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-05-26 12:14:08 +0200

    Update player.tsx

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import QRScanner from '@/components/QRScanner';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlay, faPause, faBackward, faForward, faQrcode } from '@fortawesome/free-solid-svg-icons';
-import { library, IconDefinitionOrPack } from '@fortawesome/fontawesome-svg-core';
 
-library.add([faPlay, faPause, faBackward, faForward, faQrcode] as unknown as IconDefinitionOrPack);
+library.add(faPlay, faPause, faBackward, faForward, faQrcode);
+
 
 
 
